fb99d5ad4ee5fa4274cf578bf12dfd282f453fae
Coen Steuns <c.steuns@gmail.com>
2021-06-01 00:03:49 +0200

    change the size of the mandelbrot container

--- src/components/content/Mandelbrot.svelte
+++ src/components/content/Mandelbrot.svelte
@@ -1,7 +1,43 @@
 <script>
+    import { onMount, afterUpdate, tick } from "svelte";
+    import MandelbrotDrawer from "../../utils/canvas/MandelbrotDrawer";
 
+    let canvas;
+    let container;
+
+    let width = 500;
+    let height = 500;
+
+    let mandelbrot = null;
+
+    function resizeCanvas() {
+        width = container.offsetWidth;
+        height = container.offsetHeight;
+    }
+    window.addEventListener("resize", resizeCanvas);
+
+    afterUpdate(() => {
+        mandelbrot.resetViewport();
+        mandelbrot.draw();
+    });
+
+    onMount(async () => {
+        mandelbrot = new MandelbrotDrawer(canvas);
+        resizeCanvas();
+        await tick();
+        mandelbrot.resetViewport();
+        mandelbrot.draw();
+    });
 </script>
 
-<h1>temp</h1>
+<div class="canvas-container" bind:this={container}>
+    <canvas {width} {height} bind:this={canvas} />
+</div>
+
 <style>
+    .canvas-container {
+        width: 100%;
+        height: 100vh;
+        
+    }
 </style>
