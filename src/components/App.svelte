c73f9958e4e8a2a4a52b64bf6d1b99aa7b44f1dc
Coen Steuns <c.steuns@gmail.com>
2021-05-31 23:52:12 +0200

    add mandelbrot component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,30 +1,31 @@
 <script>
     import Nav from "./Nav.svelte";
-    import Content from './Content-container.svelte'
+    import Content from "./Content-container.svelte";
     import { Router, Route } from "svelte-navigator";
+    import Mandelbrot from "./content/Mandelbrot.svelte";
 
     const navData = [
         {
             title: "2D Fractals",
             links: [
                 { title: "Mandlebrot set", url: "Mandelbrot" },
-                { title: "Julia set", url: "Julia" },
-                { title: "Tree", url: "Tree" },
+                // { title: "Julia set", url: "Julia" },
+                // { title: "Tree", url: "Tree" },
             ],
         },
-        {
-            title: "Path Finding Algorithms",
-            links: [{ title: "A Star", url: "Astar" }],
-        },
+        // {
+        //     title: "Path Finding Algorithms",
+        //     links: [{ title: "A Star", url: "Astar" }],
+        // },
     ];
 </script>
 
 <Router>
     <Nav title="Seve" data={navData} />
     <Content>
-        <Route path="Mandelbrot"><h1>mandelbrot</h1></Route>
+        <Route path="Mandelbrot"><Mandelbrot /></Route>
     </Content>
-    
 </Router>
+
 <style>
 </style>
---
+++ src/components/content/Mandelbrot.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<h1>temp</h1>
+<style>
+</style>
