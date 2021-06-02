d3e8a90acd493ac5cf28dc3028b33183196742d5
Coen Steuns <c.steuns@gmail.com>
2021-06-02 14:03:18 +0200

    added support for typescript

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
-import glslify from 'rollup-plugin-glslify';;
+import glslify from 'rollup-plugin-glslify';
+import autoPreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -42,6 +44,7 @@ export default {
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
+                preprocess: autoPreprocess()
 			}
 		}),
 		// we'll extract any component CSS out into
@@ -57,6 +60,7 @@ export default {
 			browser: true,
 			dedupe: ['svelte']
 		}),
+        typescript({sourceMap: !production}),
 		commonjs(),
         glslify(),
         
